93a67526219f0121ca9632955d4ffd768c3f7b8b
wangyingtao <wangyingtao@deepin.com>
2017-09-07 13:23:47 +0800

    add flask bootstrap

--- hello.py
+++ hello.py
@@ -9,9 +9,19 @@ from flask import abort
 from flask import url_for
 from flask import render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 @app.route('/')
 def index():
