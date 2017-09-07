c8b76199c53814cde9f9a861a8d2f1c8970a5225
wangyingtao <wangyingtao@deepin.com>
2017-09-07 16:11:50 +0800

    add flask-migrate

--- hello.py
+++ hello.py
@@ -20,6 +20,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -125,6 +126,8 @@ def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
+migrate = Migrate(app, db)
+manager.add_command("db", MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
