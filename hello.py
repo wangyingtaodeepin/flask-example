a80b5c4775778a4d5a133d3dd066bb5c52c724cc
wangyingtao <wangyingtao@deepin.com>
2017-09-07 17:14:56 +0800

    modify send mail to async

--- hello.py
+++ hello.py
@@ -22,6 +22,7 @@ from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_mail import Mail, Message
+from threading import Thread
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -138,12 +139,18 @@ class User(db.Model):
 app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
 app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <'+ app.config['FLASKY_ADMIN'] + '>'
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
 def send_mail(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
             sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role, mail=mail)
