fc1404f98996773f3b465de4fb8aecf108a8a546
wangyingtao <wangyingtao@deepin.com>
2017-09-07 12:42:39 +0800

    add simple templates

--- hello.py
+++ hello.py
@@ -7,6 +7,7 @@ from flask import make_response
 from flask import redirect
 from flask import abort
 from flask import url_for
+from flask import render_template
 from flask_script import Manager
 
 app = Flask(__name__)
@@ -17,16 +18,18 @@ def index():
     #user_agent = request.headers.get('User-Agent')
     #return '<p>Your browser is %s</p>' % user_agent
 
-    response = make_response('<h1>This document carries a cookie!</h1>')
+    #response = make_response('<h1>This document carries a cookie!</h1>')
+    #response.set_cookie('answer', '42')
+    #return response
 
-    response.set_cookie('answer', '42')
-
-    return response
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s</h1>' % name
+    #return '<h1>Hello, %s</h1>' % name
+
+    return render_template('user.html', name=name)
 
 @app.route('/redirectTest')
 def redirectTest():
