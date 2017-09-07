5a2c95468f5aaaf834ea9ca0fe89390771ef8c7e
wangyingtao <wangyingtao@deepin.com>
2017-09-07 14:17:16 +0800

    update for session and wtf FlaskForm

--- hello.py
+++ hello.py
@@ -8,12 +8,18 @@ from flask import redirect
 from flask import abort
 from flask import url_for
 from flask import render_template
+from flask import session
 from datetime import datetime
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -26,7 +32,7 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     #user_agent = request.headers.get('User-Agent')
     #return '<p>Your browser is %s</p>' % user_agent
@@ -35,7 +41,14 @@ def index():
     #response.set_cookie('answer', '42')
     #return response
 
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
@@ -56,6 +69,10 @@ def abortTest():
 @app.route('/make_responseTest')
 def make_responseTest():
     return '<h1>Hello</h1>'
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 if __name__ == '__main__':
     manager.run()
