ad49a491ceaf432266566c6658a5c57aa7693c81
wangyingtao <wangyingtao@deepin.com>
2017-09-08 15:22:58 +0800

    add change email and fix render_template when reset email is not in database.

--- app/models.py
+++ app/models.py
@@ -21,12 +21,9 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
 
     @property
     def password(self):
@@ -72,6 +69,31 @@ class User(UserMixin, db.Model):
         self.password = new_password
         db.session.add(self)
         return True
+
+    def generate_email_change_token(self, new_email, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'change_email': self.id, 'new_email': new_email})
+
+    def change_email(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('change_email') != self.id:
+            return False
+        new_email = data.get('new_email')
+        if new_email is None:
+            return False
+        if self.query.filter_by(email=new_email).first() is not None:
+            return False
+        self.email = new_email
+        db.session.add(self)
+        return True
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 @login_manager.user_loader
 def load_user(user_id):
