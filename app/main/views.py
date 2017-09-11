e5d4655886754cd2ab1b7ccc4a365546f9526989
wangyingtao <wangyingtao@deepin.com>
2017-09-11 12:29:04 +0800

    add Profiles editor

--- app/main/views.py
+++ app/main/views.py
@@ -6,7 +6,7 @@ from .. import db
 from ..models import User
 from ..email import send_email
 from . import main
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm
 from datetime import datetime
 
 @main.route('/', methods=['GET', 'POST'])
@@ -44,3 +44,27 @@ def index():
 
     return render_template('index.html', form=form, name=session.get('name'),
             known=session.get('known', False), current_time=datetime.utcnow())
+
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    return render_template('user.html', user=user)
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
