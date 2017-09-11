5a5410efa7d6c4aa7a81b90f60802d69bf5de153
wangyingtao <wangyingtao@deepin.com>
2017-09-11 14:32:34 +0800

    add Caching of user avatar hashes

--- app/main/views.py
+++ app/main/views.py
@@ -1,56 +1,21 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-from flask import render_template, session, redirect, url_for, current_app
-from .. import db
-from ..models import User
-from ..email import send_email
+from flask import render_template, redirect, url_for, abort, flash
+from flask_login import login_required, current_user
 from . import main
-from .forms import NameForm, EditProfileForm
-from datetime import datetime
+from .forms import EditProfileForm, EditProfileAdminForm
+from .. import db
+from ..models import Role, User
+from ..decorators import admin_required
 
-@main.route('/', methods=['GET', 'POST'])
+
+@main.route('/')
 def index():
-    #user_agent = request.headers.get('User-Agent')
-    #return '<p>Your browser is %s</p>' % user_agent
+    return render_template('index.html')
 
-    #response = make_response('<h1>This document carries a cookie!</h1>')
-    #response.set_cookie('answer', '42')
-    #return response
-
-    name = None
-    form = NameForm()
-
-    if form.validate_on_submit():
-        # old_name = session.get('name')
-        # if old_name is not None and old_name != form.name.data:
-        #     flash('Looks like you have changed your name!')
-
-        user = User.query.filter_by(username=form.name.data).first()
-
-        if user is None:
-            user = User(username = form.name.data)
-            db.session.add(user)
-            session['known'] = False
-            if current_app.config['FLASKY_ADMIN']:
-                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
-                        'mail/new_user', user=user)
-        else:
-            session['known'] = True
-
-        session['name'] = form.name.data
-        form.name.data = ''
-        return redirect(url_for('.index'))
-
-    return render_template('index.html', form=form, name=session.get('name'),
-            known=session.get('known', False), current_time=datetime.utcnow())
 
 
 @main.route('/user/<username>')
 def user(username):
-    user = User.query.filter_by(username=username).first()
-    if user is None:
-        abort(404)
+    user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
@@ -68,3 +33,30 @@ def edit_profile():
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', form=form)
+
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('The profile has been updated.')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
--- app/models.py
+++ app/models.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
+from datetime import datetime
+import hashlib
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app, request
 from flask_login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
 
@@ -27,15 +26,15 @@ class Role(db.Model):
     @staticmethod
     def insert_roles():
         roles = {
-                'User': (Permission.FOLLOW |
-                         Permission.COMMENT |
-                         Permission.WRITE_ARTICLES, True),
-                'Moderator': (Permission.FOLLOW |
-                              Permission.COMMENT |
-                              Permission.WRITE_ARTICLES |
-                              Permission.MODERATE_COMMENTS, False),
-                'Administrator': (0xff, False)
-                }
+            'User': (Permission.FOLLOW |
+                     Permission.COMMENT |
+                     Permission.WRITE_ARTICLES, True),
+            'Moderator': (Permission.FOLLOW |
+                          Permission.COMMENT |
+                          Permission.WRITE_ARTICLES |
+                          Permission.MODERATE_COMMENTS, False),
+            'Administrator': (0xff, False)
+        }
         for r in roles:
             role = Role.query.filter_by(name=r).first()
             if role is None:
@@ -61,7 +60,8 @@ class User(UserMixin, db.Model):
     location = db.Column(db.String(64))
     about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
-    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+    avatar_hash = db.Column(db.String(32))
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -72,6 +72,9 @@ class User(UserMixin, db.Model):
 
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = hashlib.md5(
+                self.email.encode('utf-8')).hexdigest()
 
     @property
     def password(self):
@@ -136,11 +139,14 @@ class User(UserMixin, db.Model):
         if self.query.filter_by(email=new_email).first() is not None:
             return False
         self.email = new_email
+        self.avatar_hash = hashlib.md5(
+            self.email.encode('utf-8')).hexdigest()
         db.session.add(self)
         return True
 
     def can(self, permissions):
-        return self.role is not None and (self.role.permissions & permissions) == permissions
+        return self.role is not None and \
+            (self.role.permissions & permissions) == permissions
 
     def is_administrator(self):
         return self.can(Permission.ADMINISTER)
@@ -148,6 +154,16 @@ class User(UserMixin, db.Model):
     def ping(self):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = self.avatar_hash or hashlib.md5(
+            self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, default=default, rating=rating)
 
     def __repr__(self):
         return '<User %r>' % self.username
